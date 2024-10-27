a3d0d32d2b4fa42523a4220bd82074a0621289d7
ilwad123 <ilwadabdi234@gmail.com>
2024-10-27 01:35:59 +0000

    all

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'bakery.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def reports(request):
+    return render(request, 'reports.html')
