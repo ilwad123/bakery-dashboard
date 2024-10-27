ea289137f883dcec632aa55cd454125dbb16ef72
ilwad123 <ilwadabdi234@gmail.com>
2024-10-27 01:57:34 +0000

    new

--- url.py
+++ url.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('reports/', views.reports, name='reports'),
 ]
