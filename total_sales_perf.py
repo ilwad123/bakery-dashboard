34f2617302dc48e72828ce738e42bde0bf1db56a
ilwad123 <ilwadabdi234@gmail.com>
2024-12-31 17:24:36 +0000

    Integrated Google API and finished performance CSV and all done for the database

--- total_sales_perf.py
+++ total_sales_perf.py
@@ -17,39 +17,4 @@ print(total_sales)
 
 total_sales.to_csv('performance.csv', index=False)
 
-# just add the total_distance for each driver 
-#double check the driver total_sales right in the neo4j when u add it in 
-#based on the transaction node
 
-# total_distances = {}
-
-# # # Calculate total distance for each driver per day
-# for (driver_id, date), group in grouped:
-#     group = group.sort_values(transactions['datetime'])  # Sort transactions by time
-#     locations = [bakery_location]  # Start from the bakery
-
-#     # Add transaction locations to the list
-# #     for loc in group['location']:
-# #         if pd.notna(loc):  # Ensure location is valid
-# #             lat, lon = map(float, loc.split(','))
-# #             locations.append((lat, lon))
-
-# #     # Calculate total distance for the run
-#     total_distance = 0
-#     for i in range(len(locations) - 1):
-#         total_distance += geodesic(locations[i], locations[i + 1]).meters
-
-# #     # Add to the total_distances dictionary
-# #     if driver_id not in total_distances:
-# #         total_distances[driver_id] = 0
-# #     total_distances[driver_id] += total_distance
-
-# # # Convert the results to a DataFrame
-# # distance_df = pd.DataFrame(
-# #     [{'Driver_id': driver_id, 'total_distance': dist} for driver_id, dist in total_distances.items()]
-# # )
-
-# # # Save to CSV
-# # distance_df.to_csv('performance.csv', index=False)
-
-# # print("Total distances calculated and saved to driver_total_distances.csv.")
