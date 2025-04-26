5ced12dbad6abff5fd28ac9e4b4fcfda0308c231
ilwad123 <ilwadabdi234@gmail.com>
2025-04-26 23:15:02 +0100

    5 minutes instead of 15 minutes

--- DSP_bakery/schedular.py
+++ DSP_bakery/schedular.py
@@ -7,6 +7,6 @@ while True:
     print("Checking for due cron jobs...")
     subprocess.run(["python", "manage.py", "runcrons"])
     
-    # Wait 15 minutes before checking again
-    time.sleep(900)  # 900 seconds = 15 minutes
+    # Wait 5 minutes before checking again
+    time.sleep(300)  # 300 seconds = 15 minutes
     print("Waiting for 15 minutes before next check...")
